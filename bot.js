d6a599d0aeee644ce94bd64ecb6f4e374c8b2912
botagari <48246327+botagari@users.noreply.github.com>
2019-03-05 17:28:27 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("552511824164290602")
+setInterval(function() {
+channel.send(`Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here
+Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here
+Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here
+Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking HereEizoil is Fucking Here Eizoil is Fucking Here
+Eizoil is Fucking Here Eizoil is Fucking Here Eizoil is Fucking Here`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
